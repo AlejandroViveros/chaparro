91754498ad8dca049c29c4133b90ce5c83b0476d
Alejandro Viveros <alejandroviverosm@gmail.com>
2021-03-26 16:41:51 -0300

    gatito parte 1

---
+++ js/main.js
@@ -0,0 +1,6 @@
+   //$("#gatito1").css("background-image", "url('./imagesCat/gatito.png')");
+   $("#gatito1").css("background", "red");
+   $("#gatito1").css("width", "100px");
+   $("#gatito1").css("height", "100px");
+   $("#gatito1").css("background-size", "cover");
+   $("#gatito1").css("background-image", "url('https://marvelcrowd.com/media/Captura-de-pantalla-2015-10-13-a-las-12.28.25.png')");
