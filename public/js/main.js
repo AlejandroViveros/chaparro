e471b29d3262da34b4c1d1da8cc7f03061bf425e
Carolina Vejar <vejarcarolina.95@gmail.com>
2021-03-27 22:31:02 -0300

    cambios node

--- public/js/main.js
+++ public/js/main.js
@@ -3,4 +3,4 @@
    $("#gatito1").css("width", "500px");
    $("#gatito1").css("height", "500px");
    $("#gatito1").css("background-size", "cover");
-   $("#gatito1").css("background-image", "url('https://marvelcrowd.com/media/Captura-de-pantalla-2015-10-13-a-las-12.28.25.png')");
+   $("#gatito1").css("background-image", "url('https://media.tenor.com/images/478af914bcfbd6d71dfcb65d5bc3e1b6/tenor.gif')");
